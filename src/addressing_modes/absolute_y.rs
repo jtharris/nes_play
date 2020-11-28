a7343634d23bd5d02d2696f2868b8e7f20608c93
Justin Harris <justin.t.harris@gmail.com>
2020-11-27 18:41:58 -0600

    Adding absolute Y addressing mode

---
+++ src/addressing_modes/absolute_y.rs
@@ -0,0 +1,107 @@
+use crate::cpu::{CPU, AddressingMode};
+
+pub struct AbsoluteY {
+    base_address: u16
+}
+
+impl AbsoluteY {
+    pub fn new(base_address: u16) -> Self{
+       AbsoluteY{ base_address }
+    }
+}
+
+impl AddressingMode for AbsoluteY {
+    fn read(&self, cpu: &CPU) -> u8 {
+        let address = self.base_address.wrapping_add(cpu.index_register_y as u16);
+        cpu.read_mem8(address)
+    }
+
+    fn write(&self, cpu: &mut CPU, value: u8) {
+        let address = self.base_address.wrapping_add(cpu.index_register_y as u16);
+        cpu.write_mem8(address, value);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::cpu::{CPU, AddressingMode};
+    use super::AbsoluteY;
+
+    #[test]
+    fn basic_read() {
+        // Given
+        let mut cpu = CPU::new();
+        cpu.write_mem8(0x8D, 0x09);
+        cpu.index_register_y = 0x0A;
+
+        let addressing_mode = AbsoluteY::new(0x83);
+
+        // Then
+        assert_eq!(0x09, addressing_mode.read(&cpu));
+    }
+
+    #[test]
+    fn no_wrap_around_read() {
+        // Given
+        let mut cpu = CPU::new();
+        cpu.write_mem8(0x0104, 0x29);
+        cpu.index_register_y = 0x05;
+
+        let addressing_mode = AbsoluteY::new(0x00FF);
+
+        // Then
+        assert_eq!(0x29, addressing_mode.read(&cpu));
+    }
+
+    #[test]
+    fn wrap_around_read() {
+        // Given
+        let mut cpu = CPU::new();
+        cpu.write_mem8(0x0003, 0x29);
+        cpu.index_register_y = 0x05;
+
+        let addressing_mode = AbsoluteY::new(0xFFFE);
+
+        // Then
+        assert_eq!(0x29, addressing_mode.read(&cpu));
+    }
+
+    #[test]
+    fn basic_write() {
+        // Given
+        let mut cpu = CPU::new();
+        cpu.index_register_y = 0x0A;
+
+        let addressing_mode = AbsoluteY::new(0x83);
+        addressing_mode.write(&mut cpu, 0x09);
+
+        // Then
+        assert_eq!(0x09, cpu.read_mem8(0x8D));
+    }
+
+    #[test]
+    fn no_wrap_around_write() {
+        // Given
+        let mut cpu = CPU::new();
+        cpu.index_register_y = 0x07;
+
+        let addressing_mode = AbsoluteY::new(0x00FF);
+        addressing_mode.write(&mut cpu, 0x09);
+
+        // Then
+        assert_eq!(0x09, cpu.read_mem8(0x0106));
+    }
+
+    #[test]
+    fn wrap_around_write() {
+        // Given
+        let mut cpu = CPU::new();
+        cpu.index_register_y = 0x07;
+
+        let addressing_mode = AbsoluteY::new(0xFFFD);
+        addressing_mode.write(&mut cpu, 0x09);
+
+        // Then
+        assert_eq!(0x09, cpu.read_mem8(0x0004));
+    }
+}
--- src/addressing_modes/mod.rs
+++ src/addressing_modes/mod.rs
@@ -4,3 +4,4 @@ mod zero_page_x;
 mod zero_page_y;
 mod absolute;
 mod absolute_x;
+mod absolute_y;
