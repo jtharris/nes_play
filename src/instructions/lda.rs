476bae32934bd793616932d49b70839eb8808a99
Justin Harris <justin.t.harris@gmail.com>
2020-12-22 09:48:34 -0600

    Setting S flags for LDA

--- src/instructions/lda.rs
+++ src/instructions/lda.rs
@@ -1,4 +1,4 @@
-use crate::cpu::{CPU, Instruction, AddressingMode};
+use crate::cpu::{CPU, Instruction, AddressingMode, StatusFlag};
 
 // http://www.obelisk.me.uk/6502/reference.html#LDA
 pub struct LDA {
@@ -14,25 +14,57 @@ impl LDA {
 impl Instruction for LDA {
     fn execute(&self, cpu: &mut CPU) {
         cpu.accumulator = cpu.read(&self.mode);
+
+        cpu.set_flag(StatusFlag::Zero,  cpu.accumulator == 0);
+        cpu.set_flag(StatusFlag::Negative, cpu.accumulator > 0x7F);
     }
 }
 
 #[cfg(test)]
 mod test {
     use crate::cpu::{CPU, Instruction};
-    use crate::cpu::AddressingMode::ZeroPage;
+    use crate::cpu::AddressingMode::{ZeroPage, Immediate};
     use super::LDA;
 
     #[test]
-    fn accumulator_is_loaded() {
+    fn accumulator_is_loaded_neg() {
         // Given
         let mut cpu = CPU::new();
         cpu.write(&ZeroPage(0x88), 0xF1);
 
         // When
         LDA::new(ZeroPage(0x88)).execute(&mut cpu);
-        
+
         // Then
         assert_eq!(0xF1, cpu.accumulator);
+        assert_eq!(0x80, cpu.processor_status);  // Only negative bit is set
+    }
+
+    #[test]
+    fn accumulator_is_loaded_zero() {
+        // Given
+        let mut cpu = CPU::new();
+        cpu.accumulator = 0x89;
+
+        // When
+        LDA::new(Immediate(0x00)).execute(&mut cpu);
+
+        // Then
+        assert_eq!(0x00, cpu.accumulator);
+        assert_eq!(0x02, cpu.processor_status);  // Only zero bit is set
+    }
+
+    #[test]
+    fn accumulator_is_loaded_no_flags() {
+        // Given
+        let mut cpu = CPU::new();
+        cpu.accumulator = 0x89;
+
+        // When
+        LDA::new(Immediate(0x6A)).execute(&mut cpu);
+
+        // Then
+        assert_eq!(0x6A, cpu.accumulator);
+        assert_eq!(0x00, cpu.processor_status);
     }
 }
